07942462d0c2454e93593da5125c98380066b825
TyagiManshi <166705490+TyagiManshi@users.noreply.github.com>
2024-12-23 22:58:15 +0530

    Modified layout

--- popup.js
+++ popup.js
@@ -14,20 +14,3 @@ close.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     popup.classList.remove('popup-open')
 })
-
-
-// --------------About Section---------------
-
-const tablinks = document.getElementsByClassName('tab-links')
-const tabcontents = document.getElementsByClassName('tab-contents')
-function opentab(tabname){
-    for (tablink  of tablinks) {
-        tablink.classList.remove('active-link')
-    }
-    for (tabcontent  of tabcontents) {
-        tabcontent.classList.remove('active-tab')
-    }
-    event.currentTarget.classList.add("active-link")
-    document.getElementById(tabname).classList.add("active-tab")
-}
-
